c5be250f6a3e54026751fe869bff2e955da9ad31
ravindrajain8055 <avengerravi8055@gmail.com>
2020-05-11 21:50:27 +0530

    reducers redux store & actions

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,13 @@ import Landing from './components/layout/Landing'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+// ReduX
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => 
-    <Router>
+    <Provider store={store}>
+      <Router>
       <Fragment>
         <Navbar />
         <Route exact path='/' component={ Landing } />
@@ -19,6 +23,8 @@ const App = () =>
         </section>
       </Fragment>
     </Router>
+    </Provider>
+
     
 
 export default App;
