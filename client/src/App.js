382bb69e4f6830a33c0c55ccdf439c910cd0baac
ravindrajain8055 <avengerravi8055@gmail.com>
2020-05-11 20:01:55 +0530

    Register login navbar landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-import React,{Fragment} from 'react';
+import React,{ Fragment } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 const App = () => 
     <Router>
-      <Fragment className="App">
+      <Fragment>
         <Navbar />
-        <Route exact path='/' component={ Landing } />  
+        <Route exact path='/' component={ Landing } />
+        <section className="container">
+          <Switch>
+            <Route exact path='/register' component={ Register } />
+            <Route exact path='/login' component={ Login } />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
     
