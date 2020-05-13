01dccb3625d9daf5a2fa5b44ed6092a55b5e8946
ravindrajain8055 <avengerravi8055@gmail.com>
2020-05-13 22:56:17 +0530

    profile form page

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import { loadUser } from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import CreateProfile from './components/profile-forms/CreateProfile'
+
 
 // why Run here?isnt it running in loadUser
 if(localStorage.token){
@@ -36,6 +38,7 @@ const App = () => {
             <Route exact path='/register' component={ Register } />
             <Route exact path='/login' component={ Login } />
             <PrivateRoute exact path='/dashboard' component={ Dashboard } />
+            <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
           </Switch>
         </section>
       </Fragment>
